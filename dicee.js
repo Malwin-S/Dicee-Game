8a84193685fb9bd6152ed1ff03af133e2da5fda3
Malwin-S <malwinsawant@gmail.com>
2025-09-16 15:33:31 +0530

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+    if (sessionStorage.getItem("refreshed")) {
+
+        var randomNumber1 = Math.floor(Math.random()*6)+1;
+        var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+        var img1 = document.querySelector(".img1");
+        var img2 = document.querySelector(".img2");
+
+        var heading = document.querySelector("h1");
+
+        img1.setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+        img2.setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+        if (randomNumber1 > randomNumber2) {
+            heading.innerHTML = "🚩Player 1 Wins!"
+        }
+        else if (randomNumber1 < randomNumber2) {
+            heading.innerHTML = "Player 2 Wins!🚩"
+        }
+        else {
+            heading.innerHTML = "🚩It's a Draw!🚩"
+        }
+
+    }
+
+    sessionStorage.setItem("refreshed", "true");
+
+};
